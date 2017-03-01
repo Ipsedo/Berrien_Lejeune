75bf395d35416f86ce5906409008b39318a10358
samuel <berriens@gmail.com>
2017-03-01 22:02:04 +0100

    add packages

--- src/fousfous/PlateauFousFous.java
+++ src/fousfous/PlateauFousFous.java
@@ -1,3 +1,4 @@
+package fousfous;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -5,6 +6,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
+
+import utils.Partie1;
 
 /**
  * 
