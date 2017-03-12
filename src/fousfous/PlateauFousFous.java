166b3c02ad60f48af4b56cbcc96714d286062599
samuel <berriens@gmail.com>
2017-03-12 21:11:28 +0100

    version finale

--- src/fousfous/PlateauFousFous.java
+++ src/fousfous/PlateauFousFous.java
@@ -74,14 +74,6 @@ public class PlateauFousFous implements Partie1 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		/**
-		for(int i=0; i<8; i++){
-			for(int j=0; j<8;j++){
-				System.out.println(this.plateau[i][j]);
-			}
-		}**/
-
 	}
 
 	public void saveToFile(String fileName) {
