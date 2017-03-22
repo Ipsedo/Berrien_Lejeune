ad6ce29fc6fa79ff2abaeefd3ef2ceaa8e6d5fb6
samuel <berriens@gmail.com>
2017-03-22 21:47:03 +0100

    need meilleure heuristique

--- src/fousfous/TestNegABJoueur.java
+++ src/fousfous/TestNegABJoueur.java
@@ -1,6 +1,5 @@
 package fousfous;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -112,5 +111,4 @@ public class TestNegABJoueur implements IJoueur {
 		// TODO Auto-generated method stub
 		return this.mName;
 	}
-
 }
